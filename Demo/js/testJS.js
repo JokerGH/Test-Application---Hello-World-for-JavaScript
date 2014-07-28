99384d136ca96161d3619c448417d26341e70a73
JokerGH <jokercde@gmail.com>
2014-07-28 13:08:24 +0800

    Add css and js

---
+++ Demo/js/testJS.js
@@ -0,0 +1,6 @@
+function testOnMouseOver(objDiv){
+	objDiv.innerHTML = '<div class = "innerdiv">Hello world</div>';
+}
+function testOnMouseOut(objDiv){
+	objDiv.innerHTML = '<div class = "innerdiv">Move mouse to me!</div>';
+}
